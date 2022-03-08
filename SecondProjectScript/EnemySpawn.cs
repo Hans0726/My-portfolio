56b14c5236e4b397b4bb554307213c96cc331119
Hans0726 <0726jhs@gmail.com>
2022-03-08 17:13:42 +0900

    2022 03 08 Portfolio Scripts
    2022 03 08 Portfolio Scripts

---
+++ SecondProjectScript/EnemySpawn.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject[] enemyObjects;
+    [System.NonSerialized]
+    public List<GameObject> enemyObjectPool = new List<GameObject>();
+    Transform spawnPoint;
+    int numEnemies = 20;
+
+    void Start()
+    {
+        spawnPoint = transform;
+        StartCoroutine(SpawnProcess());
+    }
+
+    public IEnumerator SpawnProcess()
+    {
+    restart:
+        int stage = GameManager.instance.stage % 5;
+        if (GameManager.instance.stage % 5 == 4)    // 미리 오브젝트를 생성하기 위해 해당 스테이지의 이 전 스테이지를 기준으로 에네미 오브젝트의 수를 정함
+            numEnemies = 1;
+        else
+            numEnemies = 20;
+
+        if (enemyObjectPool.Count != 0)
+        {
+            int objectNumBeforeLoad = enemyObjectPool.Count;
+
+            for (int i = 0;i< objectNumBeforeLoad; i++)
+            {
+                GameObject enemy = enemyObjectPool[0];
+                enemyObjectPool.Remove(enemy);
+                Destroy(enemy);
+            }
+        }
+
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            GameObject enemy = Instantiate(enemyObjects[stage], spawnPoint);
+            enemyObjectPool.Add(enemy);
+            enemy.SetActive(false);
+            if (GameManager.instance.bossStage == true)
+            {
+                SoundManager.instance.audioSourceBGM.clip = SoundManager.instance.boss;
+                SoundManager.instance.audioSourceBGM.Play();
+                break;
+            }
+        }
+
+        yield return new WaitUntil(() => GameManager.instance.stageStart == true);
+
+        while (true)
+        {
+            if (GameManager.instance.spawnStart == true)
+            {
+                if (enemyObjectPool.Count > 0)
+                {
+                    GameObject enemy = enemyObjectPool[0];
+                    enemy.transform.position = spawnPoint.position;
+                    enemy.SetActive(true);
+                    enemy.GetComponent<Enemy>().isAlive = true;
+                    enemyObjectPool.Remove(enemy);
+                    yield return new WaitForSeconds(GameManager.instance.spawnRate);
+                }
+            }
+            else
+            {   // 스폰 시간 종료 시, 오브젝트 풀 회수가 모두 완료되면
+                if (enemyObjectPool.Count == numEnemies)
+                {
+                    for (int i = 0; i < numEnemies; i++)    // 새로운 스테이지 적 생성을 위한 기존 오브젝트 소멸과 오브젝트 풀 요소 제거
+                    {
+                        GameObject enemy = enemyObjectPool[0];
+                        enemyObjectPool.Remove(enemy);
+                        Destroy(enemy);
+                    }
+                    GameManager.instance.stageStart = false;
+                    goto restart;
+                }
+            }
+            yield return null;
+        }
+    }
+}
---
+++ SecondProjectScript/Player.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    // common status
+    public PlayerStatus playerStatus;
+
+    // independent status
+    [System.NonSerialized]
+    public int hp;
+    [System.NonSerialized]
+    public int hpMax;
+    bool isAlive = true;
+    [SerializeField]
+    bool isAttacking = false;
+    SpriteRenderer sr;
+    public Queue<GameObject> attackableQueue;
+
+    Animator animator;
+    Slider playerhpSlider;
+    Text playerhpText;
+    
+    void Start()
+    {
+        hpMax = playerStatus.hpMax;
+        hp = playerStatus.hp;
+        sr = GetComponent<SpriteRenderer>();
+        attackableQueue = new Queue<GameObject>();
+        animator = GetComponent<Animator>();
+        playerhpSlider = GetComponentInChildren<Slider>();
+        playerhpText = GetComponentInChildren<Text>();
+        StartCoroutine(HealingFactor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackableQueue.Count > 0 && isAttacking == false)
+        {
+            Attack(playerStatus.attackDamage);
+        }
+        if (playerStatus.currentExp >= playerStatus.totalExp)
+        {
+            LevelUP();
+        }
+        playerhpSlider.value = (float)hp / (float)hpMax;
+    }
+    public void CheckHP()
+    {
+        playerhpText.text = hp + " / " + hpMax;
+    }
+    public void SetFalseCheckHP()
+    {
+        playerhpText.text = " ";
+    }
+    IEnumerator HealingFactor()
+    {
+        WaitForSeconds waitSec = new WaitForSeconds(5f);
+
+        while (true)
+        {
+            if (GameManager.instance.stageStart)
+                hp += playerStatus.healValue;
+
+            if (hp >= hpMax)
+                hp = hpMax;
+            yield return waitSec;
+        }
+    }
+    void Attack(int attackDamage)
+    {
+        isAttacking = true;
+        animator.SetFloat("AttackSpeed", playerStatus.attackSpeed);
+        animator.SetTrigger("Attack" + Random.Range(1, 4));
+        SoundManager.instance.audioSourceEFX.PlayOneShot(SoundManager.instance.attack[Random.Range(0,3)]);
+        StartCoroutine(AttackProcess());
+    }
+
+    IEnumerator AttackProcess()
+    {
+        Enemy enemy = attackableQueue.Peek().GetComponent<Enemy>();
+
+        while (true)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack") &&
+                animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.5f)
+            {
+                if (playerStatus.criticalRate >= Random.Range(1, 101))
+                    enemy.Damaged(playerStatus.attackDamage * 2);
+                else
+                    enemy.Damaged(playerStatus.attackDamage);
+
+                if (playerStatus.bloodsucking == true)
+                {
+                    int bloodsuckingValue = (int)((float)playerStatus.attackDamage * 0.3f);
+                    if (bloodsuckingValue < 1)
+                        hp += 1;
+                    else
+                        hp += bloodsuckingValue;
+                    if (hp >= hpMax)
+                        hp = hpMax;
+                }
+                break;
+            }
+            yield return null;
+        }
+        if (playerStatus.fastSwordTechnique)
+        {
+            int choose = Random.Range(0, 5);
+            if (choose < 2)
+                yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length * 0.5f);
+            else
+                yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length * 0.5f + 0.5f);
+        }
+        else
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length * 0.5f + 0.5f);
+        isAttacking = false;
+    }
+
+    public void Damaged(int attackDamage)
+    {
+        if (playerStatus.avoidability < Random.Range(1, 101))
+        {
+            int trueDamage = (int)((attackDamage - playerStatus.defense) * GameManager.instance.tetkaiRate);
+            if (trueDamage < 0)
+                trueDamage = 0;
+            hp -= trueDamage;
+            playerStatus.hp = hp;
+            StartCoroutine(DamagedEffect());
+            if (hp <= 0)
+                StartCoroutine(DieProcess());
+        }
+    }
+    IEnumerator DamagedEffect()
+    {
+        sr.color = Color.red;
+        yield return new WaitForSeconds(0.3f);
+        sr.color = Color.white;
+    }
+    IEnumerator DieProcess()
+    {
+        hp = 0;
+        isAlive = false;
+        foreach (GameObject enemy in attackableQueue)
+        {
+            enemy.GetComponent<Enemy>().isInQueue = false;
+            enemy.GetComponent<Enemy>().isAttacking = false;
+            enemy.GetComponent<Enemy>().player = null;
+        }
+        attackableQueue.Clear();
+
+        playerStatus.hp = hpMax;
+
+        SoundManager.instance.audioSourceEFX.PlayOneShot(SoundManager.instance.death);
+        animator.SetTrigger("Death");
+
+        float time = GameManager.instance.revivalTime;
+        UIManager.instance.clockRevival.gameObject.SetActive(true);
+        while (true)
+        {
+            time -= Time.deltaTime;
+            UIManager.instance.clockRevival.text = "플레이어 재배치까지: " + time.ToString("00") + "초";
+            if (time <= 0)
+            {
+                UIManager.instance.clockRevival.gameObject.SetActive(false);
+                break;
+            }
+            yield return null;
+        }
+        GameManager.instance.playerPositionX.Remove(transform.position.x);
+        GameManager.instance.placeableNum++;
+        Destroy(gameObject);
+    }
+
+    void LevelUP()
+    {
+        SoundManager.instance.audioSourceEFX.PlayOneShot(SoundManager.instance.levelUP);
+        playerStatus.level++;
+        playerStatus.hpMax += 5;
+        hpMax += 5;
+        playerStatus.currentExp -= playerStatus.totalExp;
+        playerStatus.skillPoints++;
+        playerStatus.totalExp *= GameManager.instance.expReinforcement;
+        UIManager.instance.cardPanel.SetActive(true);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (isAlive && attackableQueue.Count < playerStatus.attackableNum)
+            {
+                if (enemy.isInQueue == false && enemy.isAlive == true)
+                {
+                    attackableQueue.Enqueue(collision.gameObject);
+                    enemy.isInQueue = true;
+                }
+            }
+        }
+    }
+}
